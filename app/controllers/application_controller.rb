a47e450bef50c4ba329aae8fdca66b4da3a318b6
Tomer Desser <tomer.desser@wework.com>
2017-10-01 15:11:10 +0300

    Working with react component

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def index
+    render inline: '', layout: 'application'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :cuisine, :restaurants, :review
 
-
+  get '(*path)', to: 'application#index'
 end
